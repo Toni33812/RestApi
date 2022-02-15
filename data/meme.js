6ee4af13c44b669ed381f5c67a28919b2314e897
APRILIA <86938023+Toni33812@users.noreply.github.com>
2022-02-15 14:23:19 +0700

    Create meme.js

---
+++ data/meme.js
@@ -0,0 +1,22 @@
+[
+    'https://cdn-brilio-net.akamaized.net/news/2018/11/19/155181/950274-meme-anime-indonesia.jpg',
+    'https://cdn-brilio-net.akamaized.net/news/2018/11/19/155181/950270-1000xauto-meme-anime-indonesia.jpg',
+    'https://cdn-brilio-net.akamaized.net/news/2018/11/19/155181/950275-meme-anime-indonesia.jpg',
+    'https://cdn-brilio-net.akamaized.net/news/2018/11/19/155181/950276-meme-anime-indonesia.jpg',
+    'https://cdn-brilio-net.akamaized.net/news/2018/11/19/155181/950277-meme-anime-indonesia.jpg',
+    'https://cdn-brilio-net.akamaized.net/news/2018/11/19/155181/950282-meme-anime-indonesia.jpg',
+    'https://cdn-brilio-net.akamaized.net/news/2018/11/19/155181/950283-meme-anime-indonesia.jpg',
+    'https://cdn-brilio-net.akamaized.net/news/2018/11/19/155181/950284-meme-anime-indonesia.jpg',
+    'https://cdn-brilio-net.akamaized.net/news/2018/11/19/155181/950286-meme-anime-indonesia.jpg',
+    'https://cdn-brilio-net.akamaized.net/news/2018/11/19/155181/950287-meme-anime-indonesia.jpg',
+    'https://cdn-brilio-net.akamaized.net/news/2018/11/19/155181/950288-meme-anime-indonesia.jpg',
+    'https://cdn-brilio-net.akamaized.net/news/2018/11/19/155181/950293-meme-anime-indonesia.jpg',
+    'https://cdn-brilio-net.akamaized.net/news/2018/11/19/155181/950291-meme-anime-indonesia.jpg',
+    'https://cdn-brilio-net.akamaized.net/news/2018/11/19/155181/950294-meme-anime-indonesia.jpg',
+    'https://cdn-brilio-net.akamaized.net/news/2018/11/19/155181/950295-1000xauto-meme-anime-indonesia.jpg',
+    'https://cdn-brilio-net.akamaized.net/news/2018/11/19/155181/950298-meme-anime-indonesia.jpg',
+    'https://cdn-brilio-net.akamaized.net/news/2018/11/19/155181/950301-1000xauto-meme-anime-indonesia.jpg',
+    'https://cdn-brilio-net.akamaized.net/news/2018/11/19/155181/950300-meme-anime-indonesia.jpg',
+    'https://cdn-brilio-net.akamaized.net/news/2018/11/19/155181/950305-meme-anime-indonesia.jpg',
+    'https://cdn-brilio-net.akamaized.net/news/2018/11/19/155181/950304-meme-anime-indonesia.jpg'
+]
